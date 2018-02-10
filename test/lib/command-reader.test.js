4709b4640c7eeab2d5bcbfa9e057a1c0c197e0f6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-10 13:22:37 +1100

    Use async/await in CommandReader test

--- test/lib/command-reader.test.js
+++ test/lib/command-reader.test.js
@@ -3,56 +3,56 @@ const CommandReader = require('../../lib/command-reader');
 
 describe('CommandReader', () => {
 
-    it('allows user to pick and modify a past command. Commands shown last one first', () => {
+    it('allows user to pick and modify a past command. Commands shown last one first', async () => {
         const vscodeWindow = {
             showInputBox: sinon.stub().returns(Promise.resolve('COMMAND_FINAL')),
             showQuickPick: sinon.stub().returns(Promise.resolve('COMMAND_1'))
         };
         const historyStore = {getAll: () => ['COMMAND_1', 'COMMAND_2']};
         const reader = new CommandReader({historyStore, vsWindow: vscodeWindow});
-        return reader.read().then(command => {
-            expect(command).to.eql('COMMAND_FINAL');
-            expect(vscodeWindow.showQuickPick).to.have.been.calledWith(
-                ['COMMAND_2', 'COMMAND_1'],
-                {placeHolder: 'Select a command to reuse or Cancel (Esc) to write a new command'}
-            );
-            expect(vscodeWindow.showInputBox).to.have.been.calledWith({
-                placeHolder: 'Enter a command',
-                prompt: 'Edit the command if necessary',
-                value: 'COMMAND_1'
-            });
+        const command = await reader.read();
+
+        expect(command).to.eql('COMMAND_FINAL');
+        expect(vscodeWindow.showQuickPick).to.have.been.calledWith(
+            ['COMMAND_2', 'COMMAND_1'],
+            {placeHolder: 'Select a command to reuse or Cancel (Esc) to write a new command'}
+        );
+        expect(vscodeWindow.showInputBox).to.have.been.calledWith({
+            placeHolder: 'Enter a command',
+            prompt: 'Edit the command if necessary',
+            value: 'COMMAND_1'
         });
     });
 
-    it('shows inputBox right away if there is no commands recorded in the history', () => {
+    it('shows inputBox right away if there is no commands recorded in the history', async () => {
         const vscodeWindow = {
             showInputBox: sinon.stub().returns(Promise.resolve('COMMAND')),
             showQuickPick: sinon.spy()
         };
         const historyStore = {getAll: () => []};
         const reader = new CommandReader({historyStore, vsWindow: vscodeWindow});
-        return reader.read().then(command => {
-            expect(command).to.eql('COMMAND');
-            expect(vscodeWindow.showQuickPick).to.not.have.been.called;
-            expect(vscodeWindow.showInputBox).to.have.been.calledWith({
-                placeHolder: 'Enter a command',
-                prompt: 'No history available yet'
-            });
+        const command = await reader.read();
+
+        expect(command).to.eql('COMMAND');
+        expect(vscodeWindow.showQuickPick).to.not.have.been.called;
+        expect(vscodeWindow.showInputBox).to.have.been.calledWith({
+            placeHolder: 'Enter a command',
+            prompt: 'No history available yet'
         });
     });
 
-    it('shows inputBox if history command picker is dismissed', () => {
+    it('shows inputBox if history command picker is dismissed', async () => {
         const vscodeWindow = {
             showInputBox: sinon.stub().returns(Promise.resolve('COMMAND')),
             showQuickPick: () => Promise.resolve()
         };
         const historyStore = {getAll: () => ['COMMAND_1', 'COMMAND_2']};
         const reader = new CommandReader({historyStore, vsWindow: vscodeWindow});
-        return reader.read().then(command => {
-            expect(command).to.eql('COMMAND');
-            expect(vscodeWindow.showInputBox).to.have.been.calledWith({
-                placeHolder: 'Enter a command'
-            });
+        const command = await reader.read();
+
+        expect(command).to.eql('COMMAND');
+        expect(vscodeWindow.showInputBox).to.have.been.calledWith({
+            placeHolder: 'Enter a command'
         });
     });
 
