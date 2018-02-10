9b696dd6a793cfcdd019e6674f15509e97b2ac23
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-10 15:19:57 +1100

    ref #1 Read shell path from user config

---
+++ test/lib/shell-path-resolver.test.js
@@ -0,0 +1,38 @@
+
+const ShellPathResolver = require('../../lib/shell-path-resolver');
+
+describe('ShellPathResolver', () => {
+
+    const workspace = {getConfiguration: configName => {
+        const os = configName.replace('terminal.integrated.shell.', '');
+        return `${os}_SHELL_PATH`;
+    }};
+
+    it('it returns Linux shell path user specified in their config when run on Linux', () => {
+        const shellPathResolver = createShellPathResolver('linux');
+        expect(shellPathResolver.resolve()).to.eql('linux_SHELL_PATH');
+    });
+
+    it('it returns macOS shell path user specified in their config when run on macOS', () => {
+        const shellPathResolver = createShellPathResolver('darwin');
+        expect(shellPathResolver.resolve()).to.eql('osx_SHELL_PATH');
+    });
+
+    it('it returns Windows shell path user specified in their config when run on Windows', () => {
+        const shellPathResolver = createShellPathResolver('win32');
+        expect(shellPathResolver.resolve()).to.eql('windows_SHELL_PATH');
+    });
+
+    it('it returns Linux shell path user specified in their config when run on other OSs', () => {
+        const shellPathResolver = createShellPathResolver('unknown_platform');
+        expect(shellPathResolver.resolve()).to.eql('linux_SHELL_PATH');
+    });
+
+    function createShellPathResolver(platform) {
+        return new ShellPathResolver({
+            vsWorkspace: workspace,
+            platform
+        });
+    }
+
+});
