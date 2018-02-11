7c40179585d8e055433fe79d2d1ff62803407c55
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-11 16:47:34 +1100

    ref #1 WIP: Explicitly call shell programme with user defined shell args
    Instead of using `shell` option for childProcess so that approriate
    option can be passed to shell programmes, otherwise on Windows,
    even bash can be specified, they have to understand `/d /s /c` options.
    
    Also further refactored ShellCommandService test

--- test/lib/shell-command-service.test.js
+++ test/lib/shell-command-service.test.js
@@ -3,86 +3,77 @@ const ShellCommandService = require('../../lib/shell-command-service');
 
 describe('ShellCommandService', () => {
 
-    it('runs a given command on shell', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND'], Promise.resolve('COMMAND_OUTPUT'))};
-        const shellCommandExecContext = {getCwd: () => {}};
-        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
-        const params = {command: 'COMMAND_STRING'};
+    let childProcess;
+    let processRunner;
+    let shellCommandExecContext;
+    let service;
 
+    beforeEach(() => {
+        childProcess = {
+            spawn: stubWithArgs(
+                ['SHELL_PATH', ['SHELL_ARGS', 'COMMAND_STRING']], 'COMMAND',
+                ['SHELL_PATH', ['SHELL_ARGS', 'COMMAND_TEST_WITH_ENVVARS'], sinon.match({env: {SOME_ENV_VAR: '...'}})], 'COMMAND',
+                ['SHELL_PATH', ['SHELL_ARGS', 'COMMAND_TEST_WITH_EXEC_DIR'], sinon.match({cwd: 'COMMAND_EXEC_DIR'})], 'COMMAND'
+            )
+        };
+        processRunner = {
+            run: stubWithArgs(
+                ['COMMAND', undefined], Promise.resolve('COMMAND_OUTPUT'),
+                ['COMMAND', 'SELECTED_TEXT'], Promise.resolve('COMMAND_OUTPUT_TEST_WITH_INPUT')
+            )
+        };
+        shellCommandExecContext = {
+            env: {SOME_ENV_VAR: '...'},
+            getCwd: stubWithArgs(['FILE_PATH'], 'COMMAND_EXEC_DIR')
+        };
+        service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
+    });
+
+    it('runs a given command on shell', async () => {
+        const params = {command: 'COMMAND_STRING'};
         const output = await service.runCommand(params);
 
         expect(output).to.eql('COMMAND_OUTPUT');
-        expect(childProcess.spawn.args[0][1]).to.include({shell: 'SHELL_PATH'});
     });
 
     it('passes selected text in the editor to the command', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND', 'SELECTED_TEXT'], Promise.resolve('COMMAND_OUTPUT'))};
-        const shellCommandExecContext = {getCwd: () => {}};
-        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
         const params = {
             command: 'COMMAND_STRING',
             input: 'SELECTED_TEXT'
         };
+        const output = await service.runCommand(params);
 
+        expect(output).to.eql('COMMAND_OUTPUT_TEST_WITH_INPUT');
+    });
+
+    it('inherits environment variables on executing a command', async () => {
+        const params = {command: 'COMMAND_TEST_WITH_ENVVARS'};
         const output = await service.runCommand(params);
 
         expect(output).to.eql('COMMAND_OUTPUT');
     });
 
-    it('inherits environment variables on executing a command', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND'], Promise.resolve('COMMAND_OUTPUT'))};
-        const shellCommandExecContext = {
-            env: {SOME_ENV_VAR: '...'},
-            getCwd: () => {}
-        };
-        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
-        const params = {command: 'COMMAND_STRING'};
-
-        await service.runCommand(params);
-
-        const spawnArgs = childProcess.spawn.args[0];
-        expect(spawnArgs[1].env).to.eql({SOME_ENV_VAR: '...'});
-    });
-
     it('executes a command on a specific directory', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND'], Promise.resolve('COMMAND_OUTPUT'))};
-        const shellCommandExecContext = {
-            env: {SOME_ENV_VAR: '...'},
-            getCwd: stubWithArgs(['FILE_PATH'], 'COMMAND_EXEC_DIR')
-        };
-        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
         const params = {
-            command: 'COMMAND_STRING',
+            command: 'COMMAND_TEST_WITH_EXEC_DIR',
             filePath: 'FILE_PATH'
         };
-
-        await service.runCommand(params);
-
-        expect(childProcess.spawn.args[0][1]).to.include({cwd: 'COMMAND_EXEC_DIR'});
-    });
-
-    it('does not give an input to the command if no text is selected in the editor', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND', undefined], Promise.resolve('COMMAND_OUTPUT'))};
-        const shellCommandExecContext = {getCwd: () => {}};
-        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
-        const params = {command: 'COMMAND_STRING'};
-
         const output = await service.runCommand(params);
 
         expect(output).to.eql('COMMAND_OUTPUT');
     });
 
     it('throws an error if command failed', async () => {
-        const childProcess = {spawn: stubWithArgs(['COMMAND_STRING'], 'COMMAND')};
-        const processRunner = {run: stubWithArgs(['COMMAND', undefined], Promise.reject(new Error('UNEXPECTED_ERROR')))};
-        const shellCommandExecContext = {getCwd: () => {}};
+        // Cannot put this behaviour in the initial mock setup as it creates never
+        // handled promise rejections for other test cases...
+        const processRunner = {
+            run: stubWithArgs(['COMMAND', 'CAUSE_ERROR_INPUT'], Promise.reject(new Error('UNEXPECTED_ERROR')))
+        };
         const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
-        const params = {command: 'COMMAND_STRING'};
+        const params = {
+            command: 'COMMAND_STRING',
+            input: 'CAUSE_ERROR_INPUT'
+        };
 
         try {
             await service.runCommand(params);
@@ -97,7 +88,8 @@ describe('ShellCommandService', () => {
             childProcess,
             processRunner,
             shellCommandExecContext,
-            shellPathResolver: {resolve: () => 'SHELL_PATH'}
+            shellPathResolver: {resolve: () => 'SHELL_PATH'},
+            shellArgsResolver: {resolve: () => 'SHELL_ARGS'}
         });
     }
 });
