9a35c47a9f82a9f8b472917ccb61c269e7ede1d6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-13 21:39:33 +1100

    ref #1 Revert all changes that supports both custom/default shell
    As it's not necessary. This feature was introduced as specifying
    custom shell (zsh) was slow. But it turned out that specifying
    bash is not slow...
    
    Revert:
    
    * ref #1 Rename ShellProcessBuilder classes (b04314e5cb147128183b98f2298e561929ac36af)
    * ref #1 Added ProcessBuilder that uses Node's default shell (6c4935ff2a5e512e419bfeba7b34e6b16
    * ref #1 Refactor AppIntegratorFactory (c67eb504d545fa22951463ea2f9ed8a42f78c189)
    * ref #1 ProcessBuilderProvider provides appropriate builder (14fab6a5669b5c22114f5810290a026a5
    * ref #1 Inject ProcessBuilder (e2edcc207311b242af4e91b2610107bbb45e7e90)
    * ref #1 Extracted the logic of composing child process (d7dbdd5e80c960404dd90b13013606f60e86c9

--- test/lib/shell-command-service.test.js
+++ test/lib/shell-command-service.test.js
@@ -2,13 +2,19 @@
 const ShellCommandService = require('../../lib/shell-command-service');
 
 describe('ShellCommandService', () => {
-    let processBuilder;
+
+    let childProcess;
     let processRunner;
+    let shellCommandExecContext;
     let service;
 
     beforeEach(() => {
-        processBuilder = {
-            build: stubWithArgs([sinon.match({command: 'COMMAND_STRING'})], 'COMMAND')
+        childProcess = {
+            spawn: stubWithArgs(
+                ['SHELL_PATH', ['SHELL_ARG', 'COMMAND_STRING']], 'COMMAND',
+                ['SHELL_PATH', ['SHELL_ARG', 'COMMAND_TEST_WITH_ENVVARS'], sinon.match({env: {SOME_ENV_VAR: '...'}})], 'COMMAND',
+                ['SHELL_PATH', ['SHELL_ARG', 'COMMAND_TEST_WITH_EXEC_DIR'], sinon.match({cwd: 'COMMAND_EXEC_DIR'})], 'COMMAND'
+            )
         };
         processRunner = {
             run: stubWithArgs(
@@ -16,7 +22,11 @@ describe('ShellCommandService', () => {
                 ['COMMAND', 'SELECTED_TEXT'], Promise.resolve('COMMAND_OUTPUT_TEST_WITH_INPUT')
             )
         };
-        service = createShellCommandService({processBuilder, processRunner});
+        shellCommandExecContext = {
+            env: {SOME_ENV_VAR: '...'},
+            getCwd: stubWithArgs(['FILE_PATH'], 'COMMAND_EXEC_DIR')
+        };
+        service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
     });
 
     it('runs a given command on shell', async () => {
@@ -36,13 +46,30 @@ describe('ShellCommandService', () => {
         expect(output).to.eql('COMMAND_OUTPUT_TEST_WITH_INPUT');
     });
 
+    it('inherits environment variables on executing a command', async () => {
+        const params = {command: 'COMMAND_TEST_WITH_ENVVARS'};
+        const output = await service.runCommand(params);
+
+        expect(output).to.eql('COMMAND_OUTPUT');
+    });
+
+    it('executes a command on a specific directory', async () => {
+        const params = {
+            command: 'COMMAND_TEST_WITH_EXEC_DIR',
+            filePath: 'FILE_PATH'
+        };
+        const output = await service.runCommand(params);
+
+        expect(output).to.eql('COMMAND_OUTPUT');
+    });
+
     it('throws an error if command failed', async () => {
         // Cannot put this behaviour in the initial mock setup as it creates never
         // handled promise rejections for other test cases...
         const processRunner = {
             run: stubWithArgs(['COMMAND', 'CAUSE_ERROR_INPUT'], Promise.reject(new Error('UNEXPECTED_ERROR')))
         };
-        const service = createShellCommandService({processBuilder, processRunner});
+        const service = createShellCommandService({childProcess, processRunner, shellCommandExecContext});
         const params = {
             command: 'COMMAND_STRING',
             input: 'CAUSE_ERROR_INPUT'
@@ -56,10 +83,13 @@ describe('ShellCommandService', () => {
         }
     });
 
-    function createShellCommandService({processBuilder, processRunner}) {
+    function createShellCommandService({childProcess, processRunner, shellCommandExecContext}) {
         return new ShellCommandService({
+            childProcess,
             processRunner,
-            processBuilderProvider: {provide: () => processBuilder}
+            shellCommandExecContext,
+            shellProgrammeResolver: {resolve: () => 'SHELL_PATH'},
+            shellArgsRetriever: {retrieve: () => ['SHELL_ARG']}
         });
     }
 });
